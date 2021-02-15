ff11decd3c2cf366f96888abb4fe9ef90fb69ca0
Luke Loken <lukeloken@gmail.com>
2021-02-15 13:40:57 -0600

    updating file name

--- R/Setup.R
+++ R/Setup.R
@@ -7,9 +7,14 @@
 path_to_dropbox <- "C:/Users/emsta/Dropbox/MethDB2.0" #Emily's folder
 path_to_dropbox <- "C:/Users/lloken/OneDrive - DOI/GlobalRiverMethane" #Luke's USGS computer
 
-list.files(path_to_dropbox)
 # MethDB_filename <- "MethDB_EHS_18Jan21.xlsx"
 MethDB_filename <- "MethDB_EHS_25Jan21.xlsx"
+MethDB_filename <- "MethDB_EHS_5Feb21.xlsx"
+
+
+#Non set up below
+
+list.files(path_to_dropbox)
 
 
 # load formatted and converted tables into your R environment
