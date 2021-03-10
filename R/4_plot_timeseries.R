b8786d290bf8545967d02c3a063b45f0465d1bf1
Luke Loken <lukeloken@gmail.com>
2021-03-10 17:53:49 -0600

    adding a frequency figure

--- R/4_plot_timeseries.R
+++ R/4_plot_timeseries.R
@@ -132,9 +132,9 @@ CH4conc_Q_bysite <- conc_ts %>%
   theme(strip.background = element_blank(), 
         strip.text = element_text(size = 6), 
         axis.text = element_text(size = 6), 
-        plot.title = element_text(hjust = 0.5), 
-        axis.title.x = element_blank()) +
-  labs(y = expression(paste(CH[4], " (", mu, "M)"))) +
+        plot.title = element_text(hjust = 0.5)) +
+  labs(y = expression(paste(CH[4], " (", mu, "M)")), 
+       x = expression(paste("Discharge (m"^"3", " s"^"-1", ")"))) +
   ggtitle(expression(paste(CH[4], " concentrations at 20+ timepoints (title = latitude)"))) +
   guides(color = guide_legend(nrow = 1)) +
   theme(legend.position = c(0.95, -0.03),
@@ -147,3 +147,50 @@ print(CH4conc_Q_bysite)
 ggsave(file.path(path_to_dropbox, "Figures", "Timeseries", "CH4conc_Q.png"), 
        CH4conc_Q_bysite, 
        height = 10, width = 18)
+
+
+
+
+conc_f <- conc_df %>%
+  filter(!is.na(CH4mean)) %>%
+  left_join(select(sites_df, Site_Nid, Latitude)) %>%
+  mutate(Latitude_bin = case_when(Latitude >= 60 ~ "60+",
+                                  Latitude < 60 & Latitude >= 45 ~ "45 to 60", 
+                                  Latitude < 45 & Latitude >= 30 ~ "30 to 45", 
+                                  Latitude < 30 & Latitude >= 15 ~ "15 to 30", 
+                                  Latitude < 15 & Latitude >= 0 ~ "0 to 15", 
+                                  Latitude < 0 & Latitude >= -15 ~ "-15 to 0", 
+                                  Latitude < -15 & Latitude >= -30 ~ "-30 to -15", 
+                                  Latitude < -30 & Latitude >= -45 ~ "-45 to -30", 
+                                  Latitude < -45 & Latitude >= -60 ~ "-60 to -45", 
+                                  Latitude < -60 & Latitude >= 45 ~ "-60-" )) %>%
+  mutate(Latitude_bin = factor(Latitude_bin, c("-60-", "-60 to -45", 
+                                               "-45 to -30", "-30 to -15",
+                                               "-15 to 0", "0 to 15",
+                                               "15 to 30", "30 to 45",
+                                               "45 to 60", "60+")), 
+         Month = month(SampleDatestart),
+         Date2020 = as.Date(paste0("2020-", month(SampleDatestart), "-", day(SampleDatestart)))) %>%
+  group_by(Latitude_bin, Date2020) %>%
+  summarize(n_obs = n())
+
+
+conc_f_plot <- ggplot(filter(conc_f, !is.na(Latitude_bin))) +
+  theme_bw() + 
+  geom_point(aes(y=Latitude_bin, x = Date2020, size = n_obs), shape = 3, col = "darkblue", stroke = .55, alpha = 1) + 
+  scale_x_date(date_labels = "%b-%d", date_breaks = "2 months", expand = c(0.01, 0.01)) +
+  scale_y_discrete(drop = FALSE) + 
+  theme(axis.title.x = element_blank(), 
+        legend.position = "bottom", 
+        legend.text = element_text(size = 8), 
+        legend.title = element_text(size = 8)) + 
+  labs(y = "Latitude bin (deg)", size = "Number of obs") +
+  scale_size_continuous(breaks = c(1, 10, 100, 200), range = c(1, 10), trans = "sqrt")
+
+print(conc_f_plot)
+
+ggsave(file.path(path_to_dropbox, "Figures", "Timeseries", "CH4conc_frequency_by_latitude.png"), 
+       conc_f_plot, 
+       height = 4.5, width = 4)
+
+
